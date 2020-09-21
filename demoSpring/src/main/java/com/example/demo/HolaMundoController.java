16b2af09e0c0ae5514657ad33523c2f1f852cc2e
Pyckzon <Pyckzon@192.168.1.66>
2020-09-21 12:12:24 -0500

    cambio en el return del controlador

--- demoSpring/src/main/java/com/example/demo/HolaMundoController.java
+++ demoSpring/src/main/java/com/example/demo/HolaMundoController.java
@@ -8,6 +8,6 @@ public class HolaMundoController {
 	
 	@RequestMapping("/saludar")
 	public String saludar () {
-		return "Hola mundo jajajajaejejenanlsdjbfhsdbvjdncojdfosfjn";
+		return "Hola mundo!";
 	}
 }
